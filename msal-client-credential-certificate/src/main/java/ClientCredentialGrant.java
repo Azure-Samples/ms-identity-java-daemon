e3ab987ccbccec7f1760ae3bd1cc56502ef9c58d
Avery-Dunn <62066438+Avery-Dunn@users.noreply.github.com>
2020-07-27 08:23:28 -0700

    Add certificate sample (#7)
    * Restructure client credential sample
    
    * Add sample for client credential flow with a certificate
    
    * README clarifications

--- msal-client-credential-certificate/src/main/java/ClientCredentialGrant.java
+++ msal-client-credential-certificate/src/main/java/ClientCredentialGrant.java
@@ -8,21 +8,35 @@ import com.microsoft.aad.msal4j.IAuthenticationResult;
 import com.nimbusds.oauth2.sdk.http.HTTPResponse;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.Collections;
+import java.util.Properties;
 import java.util.concurrent.CompletableFuture;
 
 class ClientCredentialGrant {
 
-    private final static String TENANT_SPECIFIC_AUTHORITY = "https://login.microsoftonline.com/Enter_Tenant_Info_Here/";
-    private final static String CONFIDENTIAL_CLIENT_ID = "Enter_the_Application_Id_here";
-    private final static String CONFIDENTIAL_CLIENT_SECRET = "Enter_the_Client_Secret_Here";
-    private final static String GRAPH_DEFAULT_SCOPE = "https://graph.microsoft.com/.default";
+    private static String authority;
+    private static String clientId;
+    private static String scope;
+    private static String keyPath;
+    private static String certPath;
 
     public static void main(String args[]) throws Exception{
+
+        setUpSampleData();
 
         try {
             IAuthenticationResult result = getAccessTokenByClientCredentialGrant();
@@ -41,16 +55,23 @@ class ClientCredentialGrant {
 
     private static IAuthenticationResult getAccessTokenByClientCredentialGrant() throws Exception {
 
+        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(Files.readAllBytes(Paths.get(keyPath)));
+        PrivateKey key = KeyFactory.getInstance("RSA").generatePrivate(spec);
+
+        InputStream certStream = new ByteArrayInputStream(Files.readAllBytes(Paths.get(certPath)));
+        X509Certificate cert = (X509Certificate) CertificateFactory.getInstance("X.509").generateCertificate(certStream);
+
         ConfidentialClientApplication app = ConfidentialClientApplication.builder(
-                CONFIDENTIAL_CLIENT_ID,
-                ClientCredentialFactory.createFromSecret(CONFIDENTIAL_CLIENT_SECRET))
-                .authority(TENANT_SPECIFIC_AUTHORITY)
+                clientId,
+                ClientCredentialFactory.createFromCertificate(key, cert))
+                .authority(authority)
                 .build();
 
         // With client credentials flows the scope is ALWAYS of the shape "resource/.default", as the
         // application permissions need to be set statically (in the portal), and then granted by a tenant administrator
+
         ClientCredentialParameters clientCredentialParam = ClientCredentialParameters.builder(
-                Collections.singleton(GRAPH_DEFAULT_SCOPE))
+                Collections.singleton(scope))
                 .build();
 
         CompletableFuture<IAuthenticationResult> future = app.acquireToken(clientCredentialParam);
@@ -83,5 +104,20 @@ class ClientCredentialGrant {
             return String.format("Connection returned HTTP code: %s with message: %s",
                     httpResponseCode, conn.getResponseMessage());
         }
+    }
+
+    /**
+     * Helper function unique to this sample setting. In a real application these wouldn't be so hardcoded, for example
+     * different users may need different authority endpoints and the key/cert paths could come from a secure keyvault
+     */
+    private static void setUpSampleData() throws IOException {
+        // Load properties file and set properties used throughout the sample
+        Properties properties = new Properties();
+        properties.load(new FileInputStream(Thread.currentThread().getContextClassLoader().getResource("").getPath() + "application.properties"));
+        authority = properties.getProperty("AUTHORITY");
+        clientId = properties.getProperty("CLIENT_ID");
+        keyPath = properties.getProperty("KEY_PATH");
+        certPath = properties.getProperty("CERT_PATH");
+        scope = properties.getProperty("SCOPE");
     }
 }
