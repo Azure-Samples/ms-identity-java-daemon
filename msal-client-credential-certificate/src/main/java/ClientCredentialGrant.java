466391f5db7bb561766593c1ba4a2e6d14c87376
Avery-Dunn <avdunn@microsoft.com>
2020-10-07 18:51:08 -0700

    Remove unnecessary imports

--- msal-client-credential-certificate/src/main/java/ClientCredentialGrant.java
+++ msal-client-credential-certificate/src/main/java/ClientCredentialGrant.java
@@ -9,7 +9,6 @@ import com.nimbusds.oauth2.sdk.http.HTTPResponse;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
--- msal-client-credential-secret/src/main/java/ClientCredentialGrant.java
+++ msal-client-credential-secret/src/main/java/ClientCredentialGrant.java
@@ -8,7 +8,6 @@ import com.microsoft.aad.msal4j.IAuthenticationResult;
 import com.nimbusds.oauth2.sdk.http.HTTPResponse;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
